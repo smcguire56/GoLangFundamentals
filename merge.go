c30268f4203304cf015835ed82792550a38b3a37
smcguire56 <smcguire56@users.noreply.github.com>
2017-09-25 10:20:41 +0100

    merge added

---
+++ merge.go
@@ -0,0 +1,18 @@
+// Write a function that merges
+// two sorted lists into a new sorted list,
+// e.g. merge([1,4,6], [2,3,5]) = [1,2,3,4,5,6].
+
+package main
+
+import "fmt"
+
+func main() {
+	list1 := []byte{7, 1, 8, 47, 4, 5}
+	list2 := []byte{8, 55, 11, 47, 6, 13}
+
+	fmt.Println(merge(list1, list2))
+}
+
+func merge(list1 []byte, list2 []byte) []byte {
+    return append(list1, list2...)
+}
