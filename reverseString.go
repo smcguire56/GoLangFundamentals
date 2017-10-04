992103ee7cef6777e8c911224d89299774d29487
smcguire56 <smcguire56@users.noreply.github.com>
2017-10-04 17:28:28 +0100

    added reverse string

---
+++ reverseString.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter String: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Println(text + reverseString(text))
+}
+
+func reverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
