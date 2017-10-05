7787c36151b003a637a0304b968baf6443ffef40
smcguire56 <smcguire56@users.noreply.github.com>
2017-10-05 11:24:49 +0100

    updated time application

--- CurrentTime.go
+++ CurrentTime.go
@@ -1,3 +1,6 @@
+// A simple time application in Go.
+// https://github.com/smcguire56/GoLangFundamentals :: 2017-10-04
+// Sean McGuire
 package main
 
 import (
@@ -6,5 +9,6 @@ import (
 )
 
 func main() {
+	// Print to console the time
 	fmt.Println("The time is", time.Now())
 }
